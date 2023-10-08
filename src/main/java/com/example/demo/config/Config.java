3102d64180c24c502830e222dd6693866250113e
AVE-cesar <bertrand.ave@gmail.com>
2023-10-08 16:35:46 +0200

    1st version

--- src/main/java/com/example/demo/config/Config.java
+++ src/main/java/com/example/demo/config/Config.java
@@ -1,8 +1,41 @@
 package com.example.demo.config;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
 public class Config {
 
+	@Value("${spring.activemq.broker-url}")
+	String BROKER_URL;
+	@Value("${spring.activemq.user}")
+	String BROKER_USERNAME;
+	@Value("${spring.activemq.password}")
+	String BROKER_PASSWORD;
+
+	@Bean
+	public ActiveMQConnectionFactory connectionFactory() {
+		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+		connectionFactory.setTrustAllPackages(true);
+		connectionFactory.setBrokerURL(BROKER_URL);
+		connectionFactory.setPassword(BROKER_USERNAME);
+		connectionFactory.setUserName(BROKER_PASSWORD);
+		return connectionFactory;
+	}
+
+	@Bean
+	public JmsTemplate jmsTemplate() {
+		JmsTemplate template = new JmsTemplate();
+		template.setConnectionFactory(connectionFactory());
+
+		template.setPubSubDomain(false);
+
+		template.setDeliveryPersistent(true);
+
+		template.setReceiveTimeout(10000);
+		return template;
+	}
 }
